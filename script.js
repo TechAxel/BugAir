ad6ab540dc4ce5541207db0d61f0dcb354b0e4b5
Angelo Arcillas <angeloarcillas64@gmail.com>
2021-04-21 17:19:32 +0800

    improve upgrades indicator

--- script.js
+++ script.js
@@ -23,20 +23,23 @@ function upgrade() {
   document.querySelector(".money").innerText = stats.money;
 }
 
+function sellUpgrade() {
+  stats.money = (stats.upgradePrice - 0.5) * 100;
+  stats.upgrades -= 1;
+  document.querySelector(".upgrades").innerText = stats.upgrades;
+  document.querySelector(".money").innerText = stats.money;
+}
+
 setInterval(function () {
   stats.money = stats.money + stats.upgrades;
   document.querySelector(".money").innerText = stats.money;
 
   // upgrades indicator
   if (stats.upgrades > 0) {
-    let items = [];
-    for (i = 0; i < stats.upgrades; i++) {
-      items.push(
-        `<span class="py-2 px-3 text-white bg-black rounded">Upgrade</span>`
-      );
-    }
-    console.log("up");
-    document.querySelector("#field").innerHTML = items.join(" ");
+    const el = `<span class="py-2 px-3 text-white bg-black rounded">Upgrade</span> `;
+    document.querySelector("#field").innerHTML = el
+      .repeat(stats.upgrades)
+      .trim();
   } else {
     document.querySelector("#field").innerText = "No current upgrade.";
   }
