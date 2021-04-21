87c71a835f162b8729e1a3da5051d33484ab0755
Angelo Arcillas <angeloarcillas64@gmail.com>
2021-04-21 17:04:49 +0800

    add field upgrades indicator

--- script.js
+++ script.js
@@ -26,4 +26,18 @@ function upgrade() {
 setInterval(function () {
   stats.money = stats.money + stats.upgrades;
   document.querySelector(".money").innerText = stats.money;
+
+  // upgrades indicator
+  if (stats.upgrades > 0) {
+    let items = [];
+    for (i = 0; i < stats.upgrades; i++) {
+      items.push(
+        `<span class="py-2 px-3 text-white bg-black rounded">Upgrade</span>`
+      );
+    }
+    console.log("up");
+    document.querySelector("#field").innerHTML = items.join(" ");
+  } else {
+    document.querySelector("#field").innerText = "No current upgrade.";
+  }
 }, 1000);
