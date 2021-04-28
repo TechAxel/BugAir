3dc5efc3052593b392596a92126d0232337af6b6
TechAxel <81373230+TechAxel@users.noreply.github.com>
2021-04-28 19:50:44 -0400

    Simple Js (simplified)
    Made everything update each 1 millisecond

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ let stats = {
 
 function add(){//adds coins
   stats.coins++;//+1
-  document.getElementById("coins").innerHTML="Coins: " + stats.coins;
 }
 
 
@@ -15,13 +14,17 @@ function b1() {
         stats.ps1++;
         stats.coins = stats.coins - stats.p1;
         stats.p1 = Math.round(stats.p1 * 1.15);
-        document.getElementById("u1").innerHTML = "Price: " + stats.p1 + " Upgrades - " + stats.ps1;//updates the html
-        document.getElementById("coins").innerHTML = "Coins: " + stats.coins;//updates the html
     } else {
         alert('Not enough cows!');
     }
 }
+
 setInterval(function() {
   stats.coins = stats.coins + stats.ps1;//changes
+  setstats()
+}, 1)
+
+function setstats(){ 
+  document.getElementById("u1").innerHTML = "Price: " + stats.p1 + " Upgrades - " + stats.ps1;//updates the html
   document.getElementById("coins").innerHTML = "Coins: " + stats.coins;
-}, 1000);//every second
+}
